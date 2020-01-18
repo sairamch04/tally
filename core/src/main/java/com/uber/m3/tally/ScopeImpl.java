7130bcc8fbac9c26d4949cab84c498ebbbd2d41b
Prateek Rungta <prungta2@gmail.com>
2020-01-17 19:01:08 -0500

    Flush reporter from root scope only

--- core/src/main/java/com/uber/m3/tally/ScopeImpl.java
+++ core/src/main/java/com/uber/m3/tally/ScopeImpl.java
@@ -197,8 +197,6 @@ class ScopeImpl implements Scope {
         for (Map.Entry<String, HistogramImpl> histogram : histograms.entrySet()) {
             histogram.getValue().report(fullyQualifiedName(histogram.getKey()), tags, reporter);
         }
-
-        reporter.flush();
     }
 
     // Serializes a map to generate a key for a prefix/map combination
@@ -362,6 +360,8 @@ class ScopeImpl implements Scope {
                 subscope.report(reporter);
             }
         }
+
+        reporter.flush();
     }
 
     class ReportLoop implements Runnable {
